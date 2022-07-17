a191fe6a7de2e0beb2f2ebcccbcf483e9f5d18ed
Nitish <ntaleka@ncsu.edu>
2022-07-18 03:48:32 +0530

    Landing Page init

--- CoffeeCodersWebsite/games/index/views.py
+++ CoffeeCodersWebsite/games/index/views.py
@@ -1,4 +1,117 @@
 from django.shortcuts import render
+from django.templatetags.static import static
 
 def index(request):
-    return render(request,'index/index.html')
+    data = {
+    'live_projects' : [
+        {
+            'name' : "Pictionary",
+            'desc' : "The Game of Pictionary in a online medium",
+            'img'  : static('images/menu-1.jpg'),
+            'link' : '/abc'
+        },
+        {
+            'name' : "Pictionary2",
+            'desc' : "The Game of Pictionary in a online medium",
+            'img'  : static('images/menu-1.jpg'),
+            'link' : '/abc'
+        },
+        {
+            'name' : "Pictionary3",
+            'desc' : "The Game of Pictionary in a online medium",
+            'img'  : static('images/menu-1.jpg'),
+            'link' : '/abc'
+        }
+        ],
+    'numbers' : {
+        'years' : '2',
+        'live' : '3',
+        'total' : '10'
+    },
+    'git_projects' : [
+        [
+            {
+                'name' : 'Japenese-English Translator',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            },
+            {
+                'name' : 'Japenese-English Translator',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            },
+            {
+                'name' : 'Japenese-English Translator',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            },
+            {
+                'name' : 'Japenese-English Translator',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            }
+        ],
+        [
+            {
+                'name' : 'Japenese-English Translator22',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            },
+            {
+                'name' : 'Japenese-English Translator22',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            },
+            {
+                'name' : 'Japenese-English Translator22',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            },
+            {
+                'name' : 'Japenese-English Translator22',
+                'desc' : 'Application that Converts Japanese text to English comprehension.',
+                'topic' : 'Natural Language Processing',
+                'link' : '/abcd',
+                'icon' : 'fa fa-battery-full'
+            }
+        ]
+    ],
+    'story' : {
+        'desc' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'image' : static('images/big-logo.png'),
+    },
+    'members' : [
+        {
+            'name' : 'Nitish Talekar',
+            'desc' : 'MS in CS from NCSU',
+            'img' : static('images/nitish-profile.jpg'),
+            'link' : 'nitishtalekar.pythonanywhere.com'
+        },
+        {
+            'name' : 'Nitish Talekar',
+            'desc' : 'MS in CS from NCSU',
+            'img' : static('images/nitish-profile.jpg'),
+            'link' : 'nitishtalekar.pythonanywhere.com'
+        }
+    ],
+    'description' :{
+        'addr' : 'India - USA',
+        'mail' : 'coffeecoders10@gmail.com',
+        'github' : 'link'
+    }
+    }
+    return render(request,'index/index.html',data)
