8603780f2246d322d11ef5f0fce0c47828950e98
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2022-07-18 04:12:41 +0530

    JSON File

--- CoffeeCodersWebsite/games/index/views.py
+++ CoffeeCodersWebsite/games/index/views.py
@@ -1,117 +1,15 @@
 from django.shortcuts import render
 from django.templatetags.static import static
+import json
+from django.conf import settings
 
 def index(request):
-    data = {
-    'live_projects' : [
-        {
-            'name' : "Pictionary",
-            'desc' : "The Game of Pictionary in a online medium",
-            'img'  : static('images/menu-1.jpg'),
-            'link' : '/abc'
-        },
-        {
-            'name' : "Pictionary2",
-            'desc' : "The Game of Pictionary in a online medium",
-            'img'  : static('images/menu-1.jpg'),
-            'link' : '/abc'
-        },
-        {
-            'name' : "Pictionary3",
-            'desc' : "The Game of Pictionary in a online medium",
-            'img'  : static('images/menu-1.jpg'),
-            'link' : '/abc'
-        }
-        ],
-    'numbers' : {
-        'years' : '2',
-        'live' : '3',
-        'total' : '10'
-    },
-    'git_projects' : [
-        [
-            {
-                'name' : 'Japenese-English Translator',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            },
-            {
-                'name' : 'Japenese-English Translator',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            },
-            {
-                'name' : 'Japenese-English Translator',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            },
-            {
-                'name' : 'Japenese-English Translator',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            }
-        ],
-        [
-            {
-                'name' : 'Japenese-English Translator22',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            },
-            {
-                'name' : 'Japenese-English Translator22',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            },
-            {
-                'name' : 'Japenese-English Translator22',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            },
-            {
-                'name' : 'Japenese-English Translator22',
-                'desc' : 'Application that Converts Japanese text to English comprehension.',
-                'topic' : 'Natural Language Processing',
-                'link' : '/abcd',
-                'icon' : 'fa fa-battery-full'
-            }
-        ]
-    ],
-    'story' : {
-        'desc' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'image' : static('images/big-logo.png'),
-    },
-    'members' : [
-        {
-            'name' : 'Nitish Talekar',
-            'desc' : 'MS in CS from NCSU',
-            'img' : static('images/nitish-profile.jpg'),
-            'link' : 'nitishtalekar.pythonanywhere.com'
-        },
-        {
-            'name' : 'Nitish Talekar',
-            'desc' : 'MS in CS from NCSU',
-            'img' : static('images/nitish-profile.jpg'),
-            'link' : 'nitishtalekar.pythonanywhere.com'
-        }
-    ],
-    'description' :{
-        'addr' : 'India - USA',
-        'mail' : 'coffeecoders10@gmail.com',
-        'github' : 'link'
-    }
-    }
+    with open(settings.STATICFILES_DIRS[3] + "/json/desc.json") as json_file:
+        data = json.load(json_file)
+    print(data)
+    for i in data['live_projects']:
+        i['img'] = static(i['img'])
+    data['story']['image'] = static(data['story']['image'])
+    for i in data['members']:
+        i['img'] = static(i['img'])
     return render(request,'index/index.html',data)
