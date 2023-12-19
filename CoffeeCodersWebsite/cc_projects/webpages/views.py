0521ba0dbb88f0eab580336f1d12c4e864e867f1
wsarvesh <wsarvesh@rediffmail.com>
2023-12-19 23:47:17 +0530

    backend code

--- CoffeeCodersWebsite/cc_projects/webpages/views.py
+++ CoffeeCodersWebsite/cc_projects/webpages/views.py
@@ -1,12 +1,58 @@
 from django.shortcuts import render
+from .models import Webpages
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 def login(request):
-    return render(request,'webpages/login.html')
-    
+    if request.method == "POST":
+        password = request.POST.get('password')
+        next = request.POST.get('next')
+        if password != "council":
+            return HttpResponseRedirect('/webpages')
+        print(password, next)
+        if next == "create":
+            webpage_name = request.POST.get('webpage_name')
+            render_path = "webpages/websites/" + webpage_name + ".html"
+            path = "webpages/templates/" + render_path
+            new_webpage = Webpages(name=webpage_name, path=path, render_path=render_path)
+            new_webpage.save()
+            print(webpage_name)
+            request.session["curr_webpage"] = webpage_name
+            return HttpResponseRedirect('/webpages/editor')
+        elif next == "edit":
+            curr_webpage = request.POST.get('curr_webpage')
+            request.session["curr_webpage"] = curr_webpage
+            webpage = Webpages.objects.filter(name=curr_webpage)
+            print(curr_webpage)
+            return HttpResponseRedirect('/webpages/editor')
+
+
+    all_webpages = Webpages.objects.all()
+    print(all_webpages)
+    return render(request,'webpages/login.html', {"all_webpages": all_webpages})
+
 def editor(request):
-    return render(request,'webpages/editor.html')
-    
+    if request.method == "POST":
+        curr_webpage = request.session["curr_webpage"]
+        webpage = Webpages.objects.filter(name=curr_webpage)
+        code = request.POST.get('code')
+        print(code)
+        with open(webpage[0].path, 'w') as file:
+            file.write(code)
+    curr_webpage = request.session["curr_webpage"]
+    webpage = Webpages.objects.filter(name=curr_webpage)
+    print(webpage, webpage[0].path)
+    try:
+        with open(webpage[0].path, 'r') as file:
+            code = file.read()
+            print(code)
+    except:
+        return render(request,'webpages/editor.html')
+    print(code.strip())
+    return render(request,'webpages/editor.html', {"code": code.strip()})
+
 def webpage(request):
-    return render(request,'webpages/editor.html')
+    curr_webpage = request.session["curr_webpage"]
+    webpage = Webpages.objects.filter(name=curr_webpage)
+    return render(request, webpage[0].render_path)
