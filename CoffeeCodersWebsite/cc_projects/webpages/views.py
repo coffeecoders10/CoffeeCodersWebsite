d4f143d8f802806f23e12ac86be57d01255ccc7c
Nitish <nitishtalekar.nt503@gmail.com>
2023-12-23 20:03:32 -0600

    gitignore + other changes

--- CoffeeCodersWebsite/cc_projects/webpages/views.py
+++ CoffeeCodersWebsite/cc_projects/webpages/views.py
@@ -11,11 +11,11 @@ def login(request):
         password = request.POST.get('password')
         next = request.POST.get('next')
         if password != "council":
-            return render(request,'webpages/login.html', {"error": "Wrong Password"})
+            return render(request,'webpages/login.html', {"all_webpages": all_webpages,"error": "Wrong Password"})
         if next == "create":
             webpage_name = request.POST.get('webpage_name')
             if webpage_name == "":
-                return render(request,'webpages/login.html', {"error": "Enter Valid Website Name"})
+                return render(request,'webpages/login.html', {"all_webpages": all_webpages,"error": "Enter Valid Website Name"})
             render_path = "webpages/websites/" + webpage_name + ".html"
             path = "webpages/templates/" + render_path
             new_webpage = Webpages(name=webpage_name, path=path, render_path=render_path)
@@ -25,7 +25,7 @@ def login(request):
         elif next == "edit":
             curr_webpage = request.POST.get('curr_webpage')
             if curr_webpage == None:
-                return render(request,'webpages/login.html', {"error": "Select Valid Website Name"})
+                return render(request,'webpages/login.html', {"all_webpages": all_webpages,"error": "Select Valid Website Name"})
             request.session["curr_webpage"] = curr_webpage
             return HttpResponseRedirect('/webpages/editor')
 
@@ -42,7 +42,9 @@ def editor(request):
             code = request.POST.get('code')
             soup = BeautifulSoup(code, 'html.parser')
             html = soup.prettify()
-            with open(webpage[0].path, 'w') as file:
+            # For Testing
+            # with open(webpage[0].path, 'w') as file:
+            with open("/home/coffeecoders/CoffeeCodersWebsite/CoffeeCodersWebsite/cc_projects/" + webpage[0].path, 'w') as file:
                 file.write(html)
         else:
             webpage = Webpages.objects.filter(name=next)[0]
@@ -59,7 +61,9 @@ def editor(request):
     link = "coffeecoders.pythonanywhere.com/webpages/webpage?page="+curr_webpage
     webpage = Webpages.objects.filter(name=curr_webpage)
     try:
-        with open(webpage[0].path, 'r') as file:
+        # For Testing
+        # with open(webpage[0].path, 'r') as file:
+        with open("/home/coffeecoders/CoffeeCodersWebsite/CoffeeCodersWebsite/cc_projects/" + webpage[0].path, 'r') as file:
             code = file.read()
     except:
         return render(request,'webpages/editor.html', {"webpage_name": curr_webpage, "link":link})
